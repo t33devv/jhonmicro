2a24a80ed7fbcd9fd4b41faf4cb067cc3685dcaf
BDvok2 <105786653+BDvok2@users.noreply.github.com>
2025-08-08 23:10:06 +0300

    readded comments

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -13,13 +11,18 @@ import sqlite3
 from vacefron import Rankcard
 import time
 
+# loads the bot token from .env (create your own bot if you want to help with development)
 load_dotenv()
 token = os.getenv('TOKEN')
+
+# loads the guild token from .env (create your own guild if you want to help with development)
 guildObj = discord.Object(id=os.getenv('GUILD_ID'))
 
+# connect to "database.sqlite"
 database = sqlite3.connect('database.sqlite')
 cursor = database.cursor()
 
+# create table if it doesn't exist
 cursor.execute("""CREATE TABLE IF NOT EXISTS levels(user_id INTEGER, guild_id INTEGER, exp INTEGER, level INTEGER, last_lvl INTEGER)""")
 
 class Client(commands.Bot):
@@ -29,7 +32,13 @@ class Client(commands.Bot):
         self.leaderboard_cache_time = 0
 
     async def on_ready(self):
+        """
+            Prints the bot's username and syncs slash commands
+            (called automatically)
+        """
         print(f"ready when you are, {self.user.name}")
+
+        # sync the slash commands
         try:
             guild = discord.Object(id=os.getenv('GUILD_ID'))
             synced = await self.tree.sync(guild=guild)
@@ -37,7 +46,16 @@ class Client(commands.Bot):
         except Exception as e:
             print(f'Error syncing commands: {e}')
 
+    # ranking system
     async def on_message(self, message):
+        """
+            Adds a random amount of XP to the user who sent the message
+            (called automatically)
+
+            Args:
+                message (discord.Message):
+                    The message that was sent to trigger this function
+        """
         if message.author.bot:
             return
         
@@ -78,7 +96,19 @@ class Client(commands.Bot):
                 )
                 database.commit()
 
+    # some stupid testing stuff
     async def on_reaction_add(self, reaction, user):
+        """
+            Sends "(username) liked the message!" in the channel
+            the reaction happened in
+            (called automatically)
+
+            Args:
+                reaction (discord.Reaction):
+                    The reaction that was made to trigger this function
+                user (discord.User):
+                    The user that made the reaction
+        """
         await reaction.message.channel.send(f'{user.name} liked the message!')
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
@@ -88,16 +118,42 @@ intents.members = True
 
 client = Client(command_prefix='!', intents=intents)
 
+# below is all the slash commands
+
 @client.tree.command(name='themes', description='Suggest a theme for all following events!', guild=guildObj)
 async def themes(interaction: discord.Interaction):
+    """
+        Send a link to the theme suggestion form
+        (called when a slash command is used)
+
+        Args:
+            interaction (discord.Interaction):
+                The interaction that triggered this slash command
+    """
     await interaction.response.send_message('Suggest a theme here: https://forms.gle/HeGESheR1Pb7fPeu9')
 
 @client.tree.command(name='prerequisites', description='Suggest a prerequisite for all following events!', guild=guildObj)
 async def prerequisites(interaction: discord.Interaction):
+    """
+        Send a link to the prerequisite suggestion form
+        (called when a slash command is used)
+
+        Args:
+            interaction (discord.Interaction):
+                The interaction that triggered this slash command
+    """
     await interaction.response.send_message('Suggest a prerequisite here: https://forms.gle/eEyGmjeVzFXoed3c6')
 
 @client.tree.command(name='rules', description='Read Jam rules!', guild=guildObj)
 async def rules(interaction: discord.Interaction):
+    """
+        Send an embedded message with the server's jam rules
+        (called when a slash command is used)
+
+        Args:
+            interaction (discord.Interaction):
+                The interaction that triggered this slash command
+    """
     embed = discord.Embed(
         title="📜 Micro Jam Rules",
         description="Please follow these rules to ensure a fun and fair jam for everyone!",
@@ -113,9 +169,17 @@ async def rules(interaction: discord.Interaction):
 
     await interaction.response.send_message(embed=embed)
 
-
 @client.tree.command(name='rank', description='Check your rank!', guild=guildObj)
 async def rank(interaction: discord.Interaction):
+    """
+        Send an image showing your level,
+        and your progress to the next level
+        (called when a slash command is used)
+
+        Args:
+            interaction (discord.Interaction):
+                The interaction that triggered this slash command
+    """
     cursor.execute(
         "SELECT exp, level, last_lvl FROM levels WHERE user_id = ? AND guild_id = ?",
         (interaction.user.id, interaction.guild.id,)
@@ -144,6 +208,7 @@ async def rank(interaction: discord.Interaction):
     next_lvl_xp = int((int(level) + 1) / 0.1) ** 2
     next_lvl_xp = int(next_lvl_xp)
 
+    # use vacefron's RankCard to easily create the ranking graphic
     rank_card = Rankcard(
         username=interaction.user.display_name,
         avatar_url=interaction.user.avatar.url,
@@ -159,6 +224,10 @@ async def rank(interaction: discord.Interaction):
 
 @client.tree.command(name='leaderboard', description='Check the top 10 users in the server!', guild=guildObj)
 async def leaderboard(interaction: discord.Interaction):
+    """
+        Display the top 10 ranked users in the server
+        with caching to reduce database hits
+    """
     CACHE_DURATION = 300
     current_time = time.time()
 
@@ -169,7 +238,6 @@ async def leaderboard(interaction: discord.Interaction):
 
     print("Leaderboard: Generating new leaderboard and caching.")
     await interaction.response.defer()
-    
     
     cursor.execute('SELECT * FROM levels WHERE guild_id = ? ORDER BY level DESC, exp DESC LIMIT 10', (interaction.guild.id,))
     result = cursor.fetchall()
@@ -204,8 +272,18 @@ async def leaderboard(interaction: discord.Interaction):
 
     await interaction.followup.send(embed=embed)
 
+# joining and leaving mechanics
+
 @client.event
 async def on_member_join(member):
+    """
+        Send a welcome message in the welcome channel
+        (called automatically)
+
+        Args:
+            member (discord.User):
+                The user that joined the server
+    """
     print(f'Welcome {member.mention} to the server!')
     welcome_channel = client.get_channel(1205736676706492447)
     emoji = member.guild.get_emoji(1286138480585474198)
@@ -230,6 +308,14 @@ async def on_member_join(member):
 
 @client.event
 async def on_member_remove(member):
+    """
+        Send a leave message in the leave channel
+        (called automatically)
+
+        Args:
+            member (discord.User):
+                The user that left the server
+    """
     print(f'{member.name} has left the server!')
     leave_channel = client.get_channel(1205736676706492447)
     emoji = member.guild.get_emoji(1272841563952906260)
@@ -239,4 +325,5 @@ async def on_member_remove(member):
     await leave_channel.send(embed=embed)
 
 if __name__ == "__main__":
+    # start the bot if run directly
     client.run(token, log_handler=handler, log_level=logging.DEBUG)
